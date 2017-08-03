7b720e429b137de284c1aa512134f4cba85050d5
Hany Moh <hanymoh@msn.com>
2017-08-03 17:31:52 +0200

    Invoice js

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
   $('form').on('click', '.remove_fields', function(event) {
     $(this).prev('input[type=hidden]').val('1');
     $(this).closest('.nested_box').hide();
-    return event.preventDefault();
+    return event.originalEvent.defaultPrevented
+    // return event.preventDefault();
   });
 
   $('form').on('click', '.add_fields', function(event) {
@@ -11,13 +12,37 @@ $(document).ready(function() {
     time = new Date().getTime();
     regexp = new RegExp($(this).data('id'), 'g');
     $(this).before($(this).data('fields').replace(regexp, time));
-    return event.preventDefault();
+    return event.originalEvent.defaultPrevented
+    // return event.preventDefault();
   });
 
   $('#demoDate').datepicker({
       	format: "yyyy-mm-dd",
       	autoclose: true,
       	todayHighlight: true
-      });
+  });
 
+  function invoice() {
+    var net = parseFloat($('.invoice_total').val()) + parseFloat($('.tax').val()) - parseFloat($('.discount_value').val());
+    $('.net').val(net);
+    var rest = parseFloat($('.net').val()) - parseFloat($('.payment').val());
+    $('.rest').val(rest);
+  }
+
+
+  $('discount_or_tax').blur(function() {
+    invoice();
+  });
+
+  $(document).on('blur', '.blur', function() {
+    var box = $(this).closest(".nested_box");
+    var total = (box.find('.quantity').val() * box.find('.price').val()) - box.find('.discount_value').val();
+    box.find('.total').val(total);
+    var all = 0;
+    $('.total').each(function() {
+      all += parseFloat($(this).val());
+    });
+    $('.invoice_total').val(all);
+    invoice();
+  });
 });
